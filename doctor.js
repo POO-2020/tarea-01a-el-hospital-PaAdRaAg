e0677a3313b26ea2a150d3d57cbe4ee3e1abc413
PaAdRaAg <54459507+PaAdRaAg@users.noreply.github.com>
2020-02-18 18:53:13 -0600

    3

--- doctor.js
+++ doctor.js
@@ -1,3 +1,25 @@
 export default class Doctor{
-    
+    /**
+     * 
+     * @param {string} nombre 
+     * @param {string} especialidad 
+     * @param {number} telefono 
+     * @param {number} cedula 
+     */
+    constructor(nombre, especialidad, telefono, cedula){
+        this.nombre = nombre;
+        this. especialidad = especialidad;
+        this.telefono = telefono;
+        this.cedula = cedula;
+    }
+    getPerfil() {
+        return(`${this.nombre}, ${this.especialidad}, ${this.telefono}, ${this.cedula}`);
+    }
 }
+
+let doctor1 = new Doctor("Pedro Ramírez", "Dermatología", 3123154898, 56258456);
+let doctor2 = new Doctor("Ernesto Pérez", "Oftalmología", 3135649512, 98653212);
+let doctor3 = new Doctor("José Dominguez", "Urología", 3123566868, 63124556);
+console.log(doctor1.getPerfil());
+console.log(doctor2.getPerfil());
+console.log(doctor3.getPerfil());
--- nombre.js
+++ nombre.js
@@ -7,17 +7,18 @@ export default class Nombre{
      */
     constructor(nombre, apellidoPaterno, apellidoMaterno){
         this.nombre = nombre;
-        this.apellidoP = apellidoPaterno;
-        this.apellidoM = apellidoMaterno; 
+        this.apellidoPaterno = apellidoPaterno;
+        this.apellidoMaterno = apellidoMaterno; 
+
     }
     getNombreCompleto(){
-        return(`${this.nombre} ${this.apellidoP} ${this.apellidoM}`);
+        return(`${this.nombre} ${this.apellidoPaterno} ${this.apellidoMaterno}`);
     }
     getApellidoNombre(){
-        return(`${this.apellidoP} ${this.apellidoM} ${this.nombre}`);
+        return(`${this.apellidoPaterno} ${this.apellidoMaterno} ${this.nombre}`);
     }
     getIniciales(){
-        return(``);
+        return(`${this.nombre.charAt(0)} ${this.apellidoPaterno.charAt(0)} ${this.apellidoMaterno.charAt(0)}`);
     }
 }
 let paciente1 = new Nombre("Juan", "Solorio", "Mendoza");
